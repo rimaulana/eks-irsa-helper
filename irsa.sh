722226b80d267d407f7916cc64f32653f6f2857c
rimaulana <rimaulana@outlook.com>
2019-12-24 21:48:25 -0500

    added script source URL

--- irsa.sh
+++ irsa.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# source: https://github.com/rimaulana/eks-irsa-helper
+
 function print_help {
     echo "usage: $0 [ensure|delete] [options]"
     echo "Associate a service account with an IAM policy documents or IAM managed policy arns"
